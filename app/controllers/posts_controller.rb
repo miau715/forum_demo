70b1eda713fa0f2266ae75f0419d4731b1f5d298
miau715 <miau715@techbang.com.tw>
2011-11-23 14:37:49 +0800

    add basic user usage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, :except => [:show, :index]
   before_filter :find_board
   
   def index
@@ -14,25 +15,26 @@ class PostsController < ApplicationController
   end
   
   def edit
-    @post = @board.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
   
   def create
     @post = @board.posts.build(params[:post])
+    @post.user_id = current_user.id
     if @post.save
       redirect_to(board_post_path(@board, @post))
     end
   end  
   
   def update
-    @post = @board.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     if @post.update_attributes(params[:post])
       redirect_to(board_post_path(@board, @post))
     end
   end
   
   def destroy    
-    @post = @board.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     @post.destroy
     redirect_to(board_posts_path(@board))
   end
