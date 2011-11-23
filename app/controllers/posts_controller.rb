fd86bab6ef48a84bb9a6b20aa134b09b5c958281
miau715 <miau715@techbang.com.tw>
2011-11-23 12:19:16 +0800

    add root setting in route.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,23 @@
 class PostsController < ApplicationController
+  before_filter :find_board
+  
   def index
-    @board = Board.find(params[:board_id])
     redirect_to board_path(@board)
   end
   
   def show
-    @board = Board.find(params[:board_id])
     @post = @board.posts.find(params[:id])
   end
   
   def new
-    @board = Board.find(params[:board_id])
     @post = @board.posts.build
   end
   
   def edit
-    @board = Board.find(params[:board_id])
     @post = @board.posts.find(params[:id])
   end
   
   def create
-    @board = Board.find(params[:board_id])
     @post = @board.posts.build(params[:post])
     if @post.save
       redirect_to(board_post_path(@board, @post))
@@ -28,18 +25,21 @@ class PostsController < ApplicationController
   end  
   
   def update
-    @board = Board.find(params[:board_id])
     @post = @board.posts.find(params[:id])
     if @post.update_attributes(params[:post])
       redirect_to(board_post_path(@board, @post))
     end
   end
   
-  def destroy
-    @board = Board.find(params[:board_id])
+  def destroy    
     @post = @board.posts.find(params[:id])
     @post.destroy
     redirect_to(board_posts_path(@board))
   end
+  
+  protected
+    def find_board
+      @board = Board.find(params[:board_id])
+    end
   
 end
